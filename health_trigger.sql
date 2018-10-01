40d1b3d213d37ea99058cc06e78a9efbd012fefb
Viacheslav <viacheslavkoryagin@gmail.com>
2018-10-01 07:37:03 +0000

    Update health_trigger.sql

--- health_trigger.sql
+++ health_trigger.sql
@@ -4,13 +4,20 @@ CREATE OR REPLACE FUNCTION damage_gained() RETURNS trigger AS $damage_gained$
           or exists (select * from pvp_groups_characters where character_id = new.id)
           then
           begin
-            if (select cur_health from characters where id = new.id) > new.cur_health then
+            if (select c.cur_health from characters as c
+                join pve_groups_characters as pgc on pgc.character_id = c.id
+                where c.id = new.id and pgc.end_dt is null
+                limit 1) > new.cur_health then
               update pve_groups_characters set damage_gained = damage_gained +
                                                              ((select cur_health from characters where id = new.id) - new.cur_health);
             end if;
             if new.cur_health = 0 then
-              update pve_groups_characters set deaths = deaths + 1;
-              update pvp_groups_characters set deaths = deaths + 1;
+              update pve_groups_characters set deaths = deaths + 1
+              where character_id = new.id
+              and end_dt is null;
+              update pvp_groups_characters set deaths = deaths + 1
+              where character_id = new.id
+              and end_dt is null;
             end if;
           end;
         end if;
