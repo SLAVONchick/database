582dc4d8b6a0f23d0a323274d1e2bd157e430166
Viacheslav <viacheslavkoryagin@gmail.com>
2018-10-01 14:39:34 +0000

    min_level added to skills

--- create_tables.sql
+++ create_tables.sql
@@ -65,6 +65,7 @@ create table mmo.skills(
   name varchar(128) not null,
   practical_stats smallint not null,
   skill_type int not null references mmo.skill_types(id),
+  min_level smallint not null,
   class_id int not null references mmo.classes(id),
   is_passive bit not null,
   self bit not null,
